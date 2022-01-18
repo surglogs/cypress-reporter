12ebf69eab5fee9e4cae238a97d2e9e0bf5d8ba1
jsifalda <sifalda.jiri@gmail.com>
2022-01-18 09:10:27 +0100

    fix: correct reporter export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,3 @@
-const TestRailCache = require('./testrail.cache')
-const TestRailLogger = require('./testrail.logger')
-const TestRailNotifier = require('./testrail.notifier')
-
-import { TestRail } from './testrail'
 import { CypressTestRailReporter } from './cypress-testrail-reporter'
-
-export { CypressTestRailReporter, TestRailNotifier, TestRailCache, TestRailLogger, TestRail }
 
 export default CypressTestRailReporter
